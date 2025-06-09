209d64c84b63240a72b7456d73eb88206aa91783
Tonnie <vontonnie@gmail.com>
2025-06-09 08:02:09 +0300

    Create Shapes.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/presentation/theme/Shapes.kt
@@ -0,0 +1,17 @@
+package com.tonyxlab.notemark.presentation.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val customShapes = Shapes(
+        // Pre-Defined M3 Shapes
+        extraSmall = RoundedCornerShape(4.dp),
+        small = RoundedCornerShape(8.dp),
+        medium = RoundedCornerShape(12.dp),
+        large = RoundedCornerShape(16.dp),
+
+       // Extra-Large Override
+        extraLarge = RoundedCornerShape(20.dp)
+
+)
