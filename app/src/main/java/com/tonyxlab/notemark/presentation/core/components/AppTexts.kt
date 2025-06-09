a01c004074e907280b8aeec38d84dd643e45842b
Tonnie <vontonnie@gmail.com>
2025-06-09 08:30:16 +0300

    Add CaptionTExt composable

--- app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppTexts.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppTexts.kt
@@ -1,12 +1,23 @@
 package com.tonyxlab.notemark.presentation.core.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.tonyxlab.notemark.R
+import com.tonyxlab.notemark.presentation.core.utils.spacing
+import com.tonyxlab.notemark.presentation.theme.NoteMarkTheme
 
 @Composable
 fun AppHeaderText(
@@ -23,4 +34,40 @@ fun AppHeaderText(
             color = textColor,
             textAlign = textAlign
     )
+}
+
+@Composable
+fun AppCaptionText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textColor: Color = MaterialTheme.colorScheme.onSurfaceVariant,
+    textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    Text(
+            modifier = modifier,
+            text = text,
+            style = textStyle,
+            color = textColor,
+            textAlign = textAlign
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun AppTextFieldPreview() {
+    NoteMarkTheme {
+        Column(
+                modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxSize()
+                        .padding(MaterialTheme.spacing.spaceMedium),
+               
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spaceMedium)
+        ) {
+
+            AppHeaderText(text = stringResource(id = R.string.header_text_your_notes))
+            AppCaptionText(text = stringResource(id = R.string.header_text_your_notes))
+        }
+    }
 }
