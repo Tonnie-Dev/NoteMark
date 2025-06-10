255080ae7aa191c8ba47246b2f04870b601e4577
Tonnie <vontonnie@gmail.com>
2025-06-10 07:50:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/MainActivity.kt
@@ -3,20 +3,27 @@ package com.tonyxlab.notemark.presentation
 import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.tonyxlab.notemark.navigation.AppNavHost
+import com.tonyxlab.notemark.navigation.rememberNavOperations
+import com.tonyxlab.notemark.presentation.core.utils.spacing
 import com.tonyxlab.notemark.presentation.theme.NoteMarkTheme
+import com.tonyxlab.notemark.presentation.theme.SurfaceLowest
 import com.tonyxlab.notemark.util.DeviceType
 
 class MainActivity : ComponentActivity() {
@@ -26,35 +33,27 @@ class MainActivity : ComponentActivity() {
 
             setKeepOnScreenCondition { false }
         }
-        enableEdgeToEdge()
+
+
+        enableEdgeToEdge(
+                navigationBarStyle = SystemBarStyle.light(
+                        scrim = SurfaceLowest.toArgb(),
+                        darkScrim = SurfaceLowest.toArgb()
+                )
+        )
+
         setContent {
-            NoteMarkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                NoteMarkTheme {
 
                     val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+                val padding = MaterialTheme.spacing.spaceDefault
 
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavHost(
+                        navOperations = rememberNavOperations(),
+                        modifier = Modifier.padding(padding)
+                )
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteMarkTheme {
-        Greeting("Android")
     }
 }
