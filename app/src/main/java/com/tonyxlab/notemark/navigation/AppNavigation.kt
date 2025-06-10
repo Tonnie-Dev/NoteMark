388317281002c99eb89966046e8f733135bea12b
Tonnie <vontonnie@gmail.com>
2025-06-10 07:50:41 +0300

    Create AppNavigation.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/navigation/AppNavigation.kt
@@ -0,0 +1,50 @@
+package com.tonyxlab.notemark.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.tonyxlab.notemark.navigation.Destinations.LandingScreenDestination
+import com.tonyxlab.notemark.presentation.screens.landing.LandingScreen
+import com.tonyxlab.notemark.presentation.screens.login.LoginScreen
+import kotlinx.serialization.Serializable
+
+fun NavGraphBuilder.appDestinations(
+    navOperations: NavOperations,
+    modifier: Modifier = Modifier
+) {
+
+    composable<LandingScreenDestination> {
+
+        LandingScreen(
+                modifier = modifier,
+                navOperations = navOperations
+        )
+    }
+
+
+    composable<Destinations.LoginScreenDestination> {
+
+        LoginScreen(
+                modifier = modifier,
+                navOperations = navOperations
+        )
+    }
+
+}
+
+
+sealed class Destinations {
+
+
+    @Serializable
+    data object LandingScreenDestination : Destinations()
+
+
+    @Serializable
+    data object LoginScreenDestination : Destinations()
+
+
+    @Serializable
+    data object RegistrationScreenDestination : Destinations()
+}
