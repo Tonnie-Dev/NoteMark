c88cbc4fdc3524024945b825b40e5a613414e609
Tonnie <vontonnie@gmail.com>
2025-06-13 08:31:24 +0300

    Add LoginActionEvent interface

---
+++ app/src/main/java/com/tonyxlab/notemark/presentation/screens/login/handling/LoginActionEvent.kt
@@ -0,0 +1,8 @@
+package com.tonyxlab.notemark.presentation.screens.login.handling
+
+import com.tonyxlab.notemark.presentation.core.base.handling.ActionEvent
+
+sealed interface LoginActionEvent: ActionEvent {
+    data object NavigateToMainScreen: LoginActionEvent
+    data object NavigateToRegistrationScreen: LoginActionEvent
+}
