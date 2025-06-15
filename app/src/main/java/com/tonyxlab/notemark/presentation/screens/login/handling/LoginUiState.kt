a5db0ea0c4c0ffcf038bb0ac24c2e3b1099ee65d
Tonnie <vontonnie@gmail.com>
2025-06-15 09:39:56 +0300

    Update LoginUiState.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/screens/login/handling/LoginUiState.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/screens/login/handling/LoginUiState.kt
@@ -2,6 +2,9 @@ package com.tonyxlab.notemark.presentation.screens.login.handling
 
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.tonyxlab.notemark.presentation.core.base.handling.UiState
 
 @Stable
@@ -18,4 +21,7 @@ data class LoginUiState(
         data object InvalidEmail : FieldError
         data object InvalidPassword : FieldError
     }
+}
+class TextFieldState {
+    var text by mutableStateOf("")
 }
