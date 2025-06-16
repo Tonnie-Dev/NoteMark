18f3acb97ca4d8e9d8673ac57e676495aa24d0dc
Tonnie <vontonnie@gmail.com>
2025-06-16 08:21:07 +0300

    Create SignupUiEvent.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/presentation/screens/signup/handling/SignupUiEvent.kt
@@ -0,0 +1,11 @@
+package com.tonyxlab.notemark.presentation.screens.signup.handling
+
+import com.tonyxlab.notemark.presentation.core.base.handling.UiEvent
+import com.tonyxlab.notemark.presentation.screens.login.handling.LoginUiEvent
+
+sealed interface SignupUiEvent : UiEvent {
+    data object CreateAccount : SignupUiEvent
+    data object TogglePasswordOneVisibility: SignupUiEvent
+    data object TogglePasswordTwoVisibility: SignupUiEvent
+    data object LoginToExistingAccount : SignupUiEvent
+}
