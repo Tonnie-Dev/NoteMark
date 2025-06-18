db98eee5ba33fb8a94f5593fe3a92d3031f8b1d3
Tonnie <vontonnie@gmail.com>
2025-06-18 08:49:34 +0300

    Create SupportText.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/presentation/core/utils/SupportText.kt
@@ -0,0 +1,38 @@
+package com.tonyxlab.notemark.presentation.core.utils
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.tonyxlab.notemark.R
+
+sealed class SupportText(@StringRes val defaultText: Int, @StringRes val errorText: Int) {
+
+
+    data object UsernameSupportText : SupportText(
+            defaultText = R.string.sup_text_def_username,
+            errorText = R.string.sup_text_error_username
+    )
+
+    data object EmailSupportText : SupportText(
+            defaultText = R.string.sup_text_def_email,
+            errorText = R.string.sup_text_error_email
+    )
+
+
+    data object PasswordOneSupportText : SupportText(
+            defaultText = R.string.sup_text_def_password_one,
+            errorText = R.string.sup_text_error_password_one
+    )
+
+
+    data object PasswordTwoSupportText : SupportText(
+            defaultText = R.string.sup_text_def_password_two,
+            errorText = R.string.sup_text_error_password_two
+    )
+
+
+}
+
+
+fun SupportText.getDefaultText(context: Context): String = context.getString(defaultText)
+fun SupportText.getErrorText(context: Context): String = context.getString(errorText)
+
