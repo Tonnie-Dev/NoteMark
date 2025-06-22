2fdd8f201dd36aedb6def3a56b651ee9b9db9c0a
Tonnie <vontonnie@gmail.com>
2025-06-22 22:11:39 +0300

    Add Empty Resource Subclass

--- app/src/main/java/com/tonyxlab/notemark/domain/model/Resource.kt
+++ app/src/main/java/com/tonyxlab/notemark/domain/model/Resource.kt
@@ -4,18 +4,15 @@ sealed class Resource<out R> {
 
     data class Success<out T>(val data: T) : Resource<T>()
     data class Error(val exception: Exception) : Resource<Nothing>()
+    data object Loading: Resource<Nothing>()
+    data object Empty: Resource<Nothing>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
             is Error -> "Error[exception=$exception]"
+            is Loading -> "Loading"
+            is Empty ->  "Empty"
         }
     }
-}
-
-interface AuthRepository{
-
-    suspend fun register(registerRequest:RegisterRequest): Resource<Int>
-    suspend fun register(loginRequest:LoginRequest): Resource<Int>
-    suspend fun register(refreshRequest:RefreshRequest): Resource<Int>
 }
