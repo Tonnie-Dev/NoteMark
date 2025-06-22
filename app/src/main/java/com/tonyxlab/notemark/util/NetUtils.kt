c99ecfc761196062be25596895b9efedce1b33b7
Tonnie <vontonnie@gmail.com>
2025-06-22 12:09:02 +0300

    Create Net Utils file

---
+++ app/src/main/java/com/tonyxlab/notemark/util/NetUtils.kt
@@ -0,0 +1,15 @@
+package com.tonyxlab.notemark.util
+
+object ApiEndpoints {
+
+    // Base URL
+    const val BASE_URL = "https://notemark.pl-coding.com"
+
+    // Endpoints
+    const val REGISTRATION_ENDPOINT = "$BASE_URL/api/auth/register"
+    const val LOGIN_ENDPOINT = "$BASE_URL/auth/login"
+    const val REFRESH_ENDPOINT = "$BASE_URL/api/auth/refresh"
+}
+
+
+
