ec26c69dcb4a096f9e87515f274106fe22be7dc9
Tonnie <vontonnie@gmail.com>
2025-06-22 09:59:00 +0300

    Add Base Url/Endpoints to Constants

---
+++ app/src/main/java/com/tonyxlab/notemark/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.tonyxlab.notemark.util
+
+object Constants {
+
+    // Base URL
+    const val BASE_URL = "https://notemark.pl-coding.com/"
+
+    // Endpoints
+    const val REGISTRATION_ENDPOINT = "api/auth/register"
+    const val LOGIN_ENDPOINT = "api/auth/login"
+    const val REFRESH_ENDPOINT = "api/auth/refresh"
+}
