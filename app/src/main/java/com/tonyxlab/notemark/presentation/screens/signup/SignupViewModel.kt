529ff9d8b95917dce78fa54ce5f0f74b3816fea7
Tonnie <vontonnie@gmail.com>
2025-06-24 23:29:03 +0300

    Update SignupViewModel.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/screens/signup/SignupViewModel.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/screens/signup/SignupViewModel.kt
@@ -3,6 +3,10 @@
 package com.tonyxlab.notemark.presentation.screens.signup
 
 import androidx.compose.runtime.snapshotFlow
+import com.tonyxlab.notemark.R
+import com.tonyxlab.notemark.domain.auth.AuthRepository
+import com.tonyxlab.notemark.domain.model.RegisterRequest
+import com.tonyxlab.notemark.domain.model.Resource
 import com.tonyxlab.notemark.presentation.core.base.BaseViewModel
 import com.tonyxlab.notemark.presentation.core.utils.allFieldsFilled
 import com.tonyxlab.notemark.presentation.core.utils.checkIfError
@@ -10,20 +14,21 @@ import com.tonyxlab.notemark.presentation.core.utils.isSameAs
 import com.tonyxlab.notemark.presentation.core.utils.isValidEmail
 import com.tonyxlab.notemark.presentation.core.utils.isValidPassword
 import com.tonyxlab.notemark.presentation.core.utils.isValidUsername
+import com.tonyxlab.notemark.presentation.screens.login.handling.toText
 import com.tonyxlab.notemark.presentation.screens.signup.handling.SignupActionEvent
 import com.tonyxlab.notemark.presentation.screens.signup.handling.SignupUiEvent
 import com.tonyxlab.notemark.presentation.screens.signup.handling.SignupUiState
-import com.tonyxlab.notemark.presentation.screens.signup.handling.SignupUiState.*
+import com.tonyxlab.notemark.presentation.screens.signup.handling.SignupUiState.FieldError
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
-import okio.AsyncTimeout.Companion.condition
+import timber.log.Timber
 
 typealias SignupBaseViewModel = BaseViewModel<SignupUiState, SignupUiEvent, SignupActionEvent>
 
-class SignupViewModel : SignupBaseViewModel() {
+class SignupViewModel(private val authRepository: AuthRepository) : SignupBaseViewModel() {
 
     override val initialState: SignupUiState
         get() = SignupUiState()
@@ -35,10 +40,12 @@ class SignupViewModel : SignupBaseViewModel() {
 
     override fun onEvent(event: SignupUiEvent) {
         when (event) {
-            SignupUiEvent.CreateAccount -> createAccount()
-            SignupUiEvent.LoginToExistingAccount -> login()
             SignupUiEvent.TogglePasswordOneVisibility -> togglePasswordOne()
             SignupUiEvent.TogglePasswordTwoVisibility -> togglePasswordTwo()
+            SignupUiEvent.LoginToExistingAccount -> login()
+            SignupUiEvent.CreateAccount -> createAccount()
+            SignupUiEvent.LoginSnackbarAction -> TODO()
+            SignupUiEvent.RetrySnackbarAction -> TODO()
         }
     }
 
@@ -47,7 +54,52 @@ class SignupViewModel : SignupBaseViewModel() {
     }
 
     private fun createAccount() {
-        sendActionEvent(SignupActionEvent.NavigateToMainScreen)
+        // sendActionEvent(SignupActionEvent.NavigateToMainScreen)
+
+        launch {
+
+            updateState { it.copy(loginStatus = Resource.Loading) }
+
+            when (
+                val response =authRepository.register(
+                        registerRequest = RegisterRequest(
+                                currentState.fieldTextState.username.toText,
+                                currentState.fieldTextState.email.toText,
+                                currentState.fieldTextState.passwordOne.toText
+                        )
+                )) {
+
+                is Resource.Success -> {
+                    sendActionEvent(
+                            SignupActionEvent.ShowSnackbar(
+                                    messageRes = R.string.snack_text_signup_success,
+                                    actionLabelRes = R.string.snack_text_login,
+                                    isError = false,
+                                    onActionClick = SignupUiEvent.LoginSnackbarAction
+                            )
+                    )
+
+                    updateState { it.copy(loginStatus = Resource.Success(response.data)) }
+                    Timber.i ("Success Detected")
+                }
+
+                else -> {
+
+ updateState { it.copy(loginStatus = Resource.Error(Exception("Signup failed"))) }
+Timber.i ("Error Detected")
+                    sendActionEvent(
+                            SignupActionEvent.ShowSnackbar(
+                                    messageRes = R.string.snack_text_signup_failed,
+                                    actionLabelRes = R.string.snack_text_try_again,
+                                    isError = true,
+                                    onActionClick = SignupUiEvent.RetrySnackbarAction
+                            )
+                    )
+
+                }
+            }
+
+        }
     }
 
     private fun togglePasswordOne() {
@@ -83,16 +135,20 @@ class SignupViewModel : SignupBaseViewModel() {
 
             val usernameFlow = snapshotFlow {
                 currentState.fieldTextState.username.text
-            }.debounce(300).distinctUntilChanged()
+            }.debounce(300)
+                    .distinctUntilChanged()
             val emailFlow = snapshotFlow {
                 currentState.fieldTextState.email.text
-            }.debounce(300).distinctUntilChanged()
+            }.debounce(300)
+                    .distinctUntilChanged()
             val passwordOneFlow = snapshotFlow {
                 currentState.fieldTextState.passwordOne.text
-            }.debounce(300).distinctUntilChanged()
+            }.debounce(300)
+                    .distinctUntilChanged()
             val passwordTwoFlow = snapshotFlow {
                 currentState.fieldTextState.passwordTwo.text
-            }.debounce(300).distinctUntilChanged()
+            }.debounce(300)
+                    .distinctUntilChanged()
             combine(
                     usernameFlow,
                     emailFlow,
@@ -124,13 +180,14 @@ class SignupViewModel : SignupBaseViewModel() {
     ): FieldError {
 
         return FieldError(
-                usernameError = checkIfError  (username, ::isValidUsername),
+                usernameError = checkIfError(username, ::isValidUsername),
                 emailError = checkIfError(email, ::isValidEmail),
-                passwordError =checkIfError (passwordOne, ::isValidPassword),
-                confirmPasswordError = checkIfError (passwordTwo){ passwordOne isSameAs passwordTwo},
-                allFieldsFilled = allFieldsFilled(username,email, passwordOne, passwordTwo)
+                passwordError = checkIfError(passwordOne, ::isValidPassword),
+                confirmPasswordError = checkIfError(passwordTwo) { passwordOne isSameAs passwordTwo },
+                allFieldsFilled = allFieldsFilled(username, email, passwordOne, passwordTwo)
         )
     }
+
 
 
 }
