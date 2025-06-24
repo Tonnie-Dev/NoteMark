fa1e021f58eaa5e3470e879bd087232635af8aaa
Tonnie <vontonnie@gmail.com>
2025-06-24 23:29:47 +0300

    Update SignupActionEvent.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/screens/signup/handling/SignupActionEvent.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/screens/signup/handling/SignupActionEvent.kt
@@ -1,8 +1,15 @@
 package com.tonyxlab.notemark.presentation.screens.signup.handling
 
+import androidx.annotation.StringRes
 import com.tonyxlab.notemark.presentation.core.base.handling.ActionEvent
 
-sealed interface SignupActionEvent: ActionEvent {
-    data object NavigateToMainScreen: SignupActionEvent
-    data object NavigateToLoginScreen: SignupActionEvent
+sealed interface SignupActionEvent : ActionEvent {
+    data object NavigateToLoginScreen : SignupActionEvent
+    data class ShowSnackbar(
+        @StringRes val messageRes: Int,
+        @StringRes val actionLabelRes: Int? = null,
+        val onActionClick: SignupUiEvent? = null,
+        val isError: Boolean = false,
+    ) : SignupActionEvent
+
 }
