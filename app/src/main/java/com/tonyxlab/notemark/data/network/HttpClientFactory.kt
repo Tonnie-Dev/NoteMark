e5ffd02adef1b8fde39dae62d11c1c75cbaf9405
Tonnie <vontonnie@gmail.com>
2025-07-05 14:38:24 +0300

    Update HttpClientFactory.kt

--- app/src/main/java/com/tonyxlab/notemark/data/network/HttpClientFactory.kt
+++ app/src/main/java/com/tonyxlab/notemark/data/network/HttpClientFactory.kt
@@ -2,9 +2,9 @@ package com.tonyxlab.notemark.data.network
 
 import com.tonyxlab.notemark.BuildConfig
 import com.tonyxlab.notemark.data.local.datastore.TokenStorage
-import com.tonyxlab.notemark.data.remote.auth.AccessTokenRequest
-import com.tonyxlab.notemark.data.remote.auth.AccessTokenResponse
+import com.tonyxlab.notemark.data.remote.auth.dto.AccessTokenResponse
 import com.tonyxlab.notemark.util.ApiEndpoints
+import com.tonyxlab.notemark.util.Constants.EMAIL_HEADER_KEY
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
@@ -18,90 +18,97 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.client.request.post
-import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import timber.log.Timber
 
+class HttpClientFactory {
 
-fun provideDefaultHttpClient(): HttpClient {
+    private val defaultClient = HttpClient(CIO) {
 
-    return HttpClient(CIO) {
         install(ContentNegotiation) {
-            json()
+            json(Json { ignoreUnknownKeys = true })
+        }
+
+        defaultRequest {
+            contentType(ContentType.Application.Json)
+            header(EMAIL_HEADER_KEY, BuildConfig.USER_EMAIL)
+           // header("Debug", true)
         }
     }
-}
-
-fun provideMainHttpClient(): HttpClient {
-
-    return HttpClient(CIO) {
 
 
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
-        }
-        install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Timber.tag("ApiRequest")
-                            .d(message)
-                }
+    fun provideMainHttpClient(): HttpClient {
 
+        return HttpClient(CIO) {
+
+
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
             }
-            level = LogLevel.ALL
-        }
-        install(Auth) {
-            bearer {
-                loadTokens {
-                    val access = TokenStorage.getAccessToken()
-                    val refresh = TokenStorage.getRefreshToken()
-
-                    if (access != null && refresh != null) {
-                        BearerTokens(access, refresh)
-                    } else {
-                        null
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Timber.tag("ApiRequest")
+                                .d(message)
                     }
+
                 }
+                level = LogLevel.ALL
+            }
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        val access = TokenStorage.getAccessToken()
+                        val refresh = TokenStorage.getRefreshToken()
 
-                refreshTokens {
-                    val refreshToken = TokenStorage.getRefreshToken() ?: return@refreshTokens null
-
-                    try {
-
-
-                        val refreshResponse = client.post(ApiEndpoints.REFRESH_ENDPOINT) {
-                            header("Authorization", "Bearer $refreshToken")
-
-                        }
-
-
-                        if (refreshResponse.status == HttpStatusCode.OK) {
-                            val newTokens = refreshResponse.body<AccessTokenResponse>()
-                            TokenStorage.saveTokens(
-                                    accessToken = newTokens.accessToken,
-                                    refreshToken = newTokens.refreshToken,
-                                    username = newTokens.username
-                            )
-                            BearerTokens(newTokens.accessToken, newTokens.refreshToken)
+                        if (access != null && refresh != null) {
+                            BearerTokens(access, refresh)
                         } else {
+                            null
+                        }
+                    }
+
+                    refreshTokens {
+                        val refreshToken =
+                            TokenStorage.getRefreshToken() ?: return@refreshTokens null
+
+                        try {
+
+
+                            val refreshResponse =
+                                defaultClient.post(ApiEndpoints.REFRESH_ENDPOINT) {
+                                    header("Authorization", "Bearer $refreshToken")
+
+                                }
+
+
+                            if (refreshResponse.status == HttpStatusCode.OK) {
+                                val newTokens = refreshResponse.body<AccessTokenResponse>()
+                                TokenStorage.saveTokens(
+                                        accessToken = newTokens.accessToken,
+                                        refreshToken = newTokens.refreshToken,
+                                        username = newTokens.username
+                                )
+                                BearerTokens(newTokens.accessToken, newTokens.refreshToken)
+                            } else {
+                                TokenStorage.clearTokens()
+                                null
+                            }
+                        } catch (e: Exception) {
                             TokenStorage.clearTokens()
                             null
                         }
-                    } catch (e: Exception) {
-                        TokenStorage.clearTokens()
-                        null
                     }
                 }
             }
         }
     }
+
+
 }
-
-
