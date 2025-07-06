37be05835a767455e6dba53c665b0935935b3f84
Tonnie <vontonnie@gmail.com>
2025-07-06 15:48:14 +0300

    Create NoteItem.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/domain/model/NoteItem.kt
@@ -0,0 +1,11 @@
+package com.tonyxlab.notemark.domain.model
+
+import com.tonyxlab.notemark.util.Constants
+import java.time.LocalDateTime
+
+data class NoteItem(
+    val id: Long = Constants.INITIAL_DATABASE_ID,
+    val title: String = "“New Note",
+    val content: String = "",
+    val createdOn: LocalDateTime,
+)
