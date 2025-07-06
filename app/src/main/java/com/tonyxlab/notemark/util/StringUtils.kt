dfe4bef815fce1d0276f8c8ac6e6f854f65ed475
Tonnie <vontonnie@gmail.com>
2025-07-06 09:38:38 +0300

    Add generateLoremIpsum utility function

--- app/src/main/java/com/tonyxlab/notemark/util/StringUtils.kt
+++ app/src/main/java/com/tonyxlab/notemark/util/StringUtils.kt
@@ -21,3 +21,23 @@ fun formatUserInitials(username: String): String {
 }
 
 
+fun generateLoremIpsum(wordCount: Int): String {
+    val loremIpsumWords =
+        listOf(
+                "Lorem",
+                "ipsum",
+                "dolor",
+                "sit",
+                "amet",
+                "consectetur",
+                "adipiscing",
+                "elit",
+                "sed",
+                "do",)
+
+    return (0 until  wordCount).joinToString( " ") {
+
+        loremIpsumWords.random().replaceFirstChar {it.titlecase()}
+    }
+
+}
