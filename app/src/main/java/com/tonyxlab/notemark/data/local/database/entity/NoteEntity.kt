37a4fc67601a2eeec1375e567114a14d8087ce13
Tonnie <vontonnie@gmail.com>
2025-07-06 15:48:10 +0300

    Create NoteEntity.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/data/local/database/entity/NoteEntity.kt
@@ -0,0 +1,18 @@
+package com.tonyxlab.notemark.data.local.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "content")
+    val content: String,
+    @ColumnInfo(name = "created_on")
+    val createdOn: Long,
+)
