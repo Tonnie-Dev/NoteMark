aed61e8d91762710fd3209d47b03e6eb70f69a9a
Tonnie <vontonnie@gmail.com>
2025-07-06 12:25:01 +0300

    Add text trimming utilities and remove string tests

--- app/src/main/java/com/tonyxlab/notemark/util/StringUtils.kt
+++ app/src/main/java/com/tonyxlab/notemark/util/StringUtils.kt
@@ -1,9 +1,14 @@
 package com.tonyxlab.notemark.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
 
 fun formatUserInitials(username: String): String {
 
-    val stringsList = username.trim().split(" ").filter { it.isNotBlank() }
+    val stringsList = username.trim()
+            .split(" ")
+            .filter { it.isNotBlank() }
 
     return when (stringsList.size) {
         0 -> ""
@@ -12,8 +17,12 @@ fun formatUserInitials(username: String): String {
                 .take(2)
 
         else -> {
-            val firstInitial = stringsList.first().first().uppercaseChar()
-            val lastInitial = stringsList.last().first().uppercaseChar()
+            val firstInitial = stringsList.first()
+                    .first()
+                    .uppercaseChar()
+            val lastInitial = stringsList.last()
+                    .first()
+                    .uppercaseChar()
             "$firstInitial$lastInitial"
         }
     }
@@ -33,11 +42,42 @@ fun generateLoremIpsum(wordCount: Int): String {
                 "adipiscing",
                 "elit",
                 "sed",
-                "do",)
+                "do",
+        )
 
-    return (0 until  wordCount).joinToString( " ") {
+    return (0 until wordCount).joinToString(" ") {
 
-        loremIpsumWords.random().replaceFirstChar {it.titlecase()}
+        loremIpsumWords.random()
+                .replaceFirstChar { it.titlecase() }
     }
 
 }
+
+
+fun trimTextToFit(content: String, maxCharacters: Int): String {
+    return if (content.length > maxCharacters) {
+        content.take(maxCharacters).trimEnd() + "…"
+    } else content
+}
+
+
+@Composable
+fun trimContentText(deviceType: DeviceType, content: String): String {
+    val maxTextCharacters = remember(deviceType) {
+        when (deviceType) {
+            DeviceType.MOBILE_PORTRAIT,
+            DeviceType.MOBILE_LANDSCAPE -> 150
+
+            DeviceType.TABLET_PORTRAIT,
+            DeviceType.TABLET_LANDSCAPE -> 250
+
+            else -> 250
+        }
+    }
+
+    return remember(content, maxTextCharacters) {
+        trimTextToFit(content, maxTextCharacters)
+    }
+}
+
+
