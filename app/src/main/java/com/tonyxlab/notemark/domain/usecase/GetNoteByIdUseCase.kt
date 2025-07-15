398eac4f01b81205655ddf9335b75129ab66fcd8
Tonnie <vontonnie@gmail.com>
2025-07-15 20:13:19 +0300

    Create GetNoteByIdUseCase.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/domain/usecase/GetNoteByIdUseCase.kt
@@ -0,0 +1,21 @@
+package com.tonyxlab.notemark.domain.usecase
+
+import com.tonyxlab.notemark.domain.exception.NoteNotFoundException
+import com.tonyxlab.notemark.domain.model.NoteItem
+import com.tonyxlab.notemark.domain.model.Resource
+import com.tonyxlab.notemark.domain.repository.NoteRepository
+
+class GetNoteByIdUseCase(private val repository: NoteRepository) {
+
+    suspend operator fun invoke(id: Long): NoteItem {
+
+        return repository.getNoteById(id)
+                .let { result ->
+                    when (result) {
+                        is Resource.Success -> result.data
+                        is Resource.Error -> throw result.exception
+                        else -> throw NoteNotFoundException(id = id)
+                    }
+                }
+    }
+}
