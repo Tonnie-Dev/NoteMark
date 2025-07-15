9aa0993ee042c308a6fcf479b127dced7a2c716a
Tonnie <vontonnie@gmail.com>
2025-07-15 20:13:24 +0300

    Create UpsertNoteUseCase.kt

---
+++ app/src/main/java/com/tonyxlab/notemark/domain/usecase/UpsertNoteUseCase.kt
@@ -0,0 +1,16 @@
+package com.tonyxlab.notemark.domain.usecase
+
+import com.tonyxlab.notemark.domain.model.NoteItem
+import com.tonyxlab.notemark.domain.model.Resource
+import com.tonyxlab.notemark.domain.repository.NoteRepository
+
+class UpsertNoteUseCase(private val repository: NoteRepository) {
+
+    suspend operator fun invoke(noteItem: NoteItem): Long {
+        return when (val result = repository.upsertNote(noteItem)) {
+            is Resource.Success -> result.data
+            is Resource.Error -> throw result.exception
+            else -> throw Exception("Unknow error while upserting note")
+        }
+    }
+}
