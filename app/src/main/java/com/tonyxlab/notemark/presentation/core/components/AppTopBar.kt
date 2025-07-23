665d2ebb46416cd6cd1f373099df5ff9a953559a
Tonnie <vontonnie@gmail.com>
2025-07-23 18:33:29 +0300

    Update AppTopBar.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppTopBar.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppTopBar.kt
@@ -1,92 +1,84 @@
 package com.tonyxlab.notemark.presentation.core.components
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.tonyxlab.notemark.R
 import com.tonyxlab.notemark.presentation.core.utils.spacing
 import com.tonyxlab.notemark.presentation.theme.NoteMarkTheme
-import com.tonyxlab.notemark.R
+import com.tonyxlab.notemark.presentation.theme.textButtonStyle
+
 @Composable
 fun AppTopBar(
-    profileInitials: String,
-    modifier: Modifier = Modifier,
-    @StringRes titleRes: Int = R.string.app_name,
+    screenTitle: String,
+    onPressBack: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    val clipShape = MaterialTheme.shapes.medium
 
     Surface(color = MaterialTheme.colorScheme.background) {
 
         Row(
                 modifier = modifier
-                        .statusBarsPadding()
                         .fillMaxWidth()
-                        .height(MaterialTheme.spacing.spaceExtraLarge)
-                        .padding(vertical = MaterialTheme.spacing.spaceSmall)
-                        .padding(horizontal = MaterialTheme.spacing.spaceMedium),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                        .height(MaterialTheme.spacing.spaceSmall * 7)
+                        .padding(start = MaterialTheme.spacing.spaceMedium),
+                verticalAlignment = Alignment.CenterVertically
         ) {
 
-            Text(
-                    text = stringResource(id = titleRes),
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onSurface
+            Icon(
+                    modifier = Modifier
+                            .padding(end = MaterialTheme.spacing.spaceDoubleDp)
+                            .clickable {onPressBack()},
+                    imageVector = Icons.Default.ChevronLeft,
+                    contentDescription = stringResource(id = R.string.cds_text_back),
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
             )
 
-            Box(
-                    modifier = Modifier
-                            .clip(clipShape)
-                            .background(
-                                    color = MaterialTheme.colorScheme.primary,
-                                    shape = clipShape
-                            )
-                            .size(MaterialTheme.spacing.spaceTen * 4)
-                            .padding(MaterialTheme.spacing.spaceExtraSmall),
-                    contentAlignment = Alignment.Center
-            ) {
-                Text(
-                        text = profileInitials,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.onPrimary
-                )
-            }
+            Text(
+                    text = screenTitle,
+                    style = MaterialTheme.typography.textButtonStyle.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
+            )
+
         }
     }
+
+
 }
 
 @PreviewLightDark
 @Composable
-private fun AppTopBarPreview() {
+private fun AppTopBar_Preview() {
+
 
     NoteMarkTheme {
 
         Column(
                 modifier = Modifier
-                        .background(color = MaterialTheme.colorScheme.surface)
                         .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.surface)
         ) {
+
             AppTopBar(
-                    titleRes = R.string.app_name,
-                    profileInitials = "TX"
+                    screenTitle = "SETTINGS",
+                    onPressBack = {}
             )
+
         }
     }
 }
