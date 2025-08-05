04e3d7fe4789c4d8ee7de0b37dcdd58964203035
Tonnie <vontonnie@gmail.com>
2025-08-05 21:34:50 +0300

    Update ComposeUtils.kt

--- app/src/main/java/com/tonyxlab/notemark/util/ComposeUtils.kt
+++ app/src/main/java/com/tonyxlab/notemark/util/ComposeUtils.kt
@@ -1,20 +1,27 @@
 package com.tonyxlab.notemark.util
 
 import android.app.Activity
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.IntSize
 import androidx.core.view.WindowCompat
 import timber.log.Timber
 
 inline fun Modifier.ifThen(flag: Boolean, modifierBuilder: Modifier.() -> Modifier): Modifier =
     if (flag) this.modifierBuilder() else this
+
+
 @Composable
 fun SetStatusBarIconsColor(darkIcons: Boolean) {
-    Timber.tag("ComposeUtils").i("SetStatusBarIconsColor called with darkIcons = $darkIcons")
+    Timber.tag("ComposeUtils")
+            .i("SetStatusBarIconsColor called with darkIcons = $darkIcons")
 
     val view = LocalView.current
 
@@ -26,7 +33,28 @@ fun SetStatusBarIconsColor(darkIcons: Boolean) {
 
     SideEffect {
         insetsController?.isAppearanceLightStatusBars = darkIcons
-        Timber.tag("ComposeUtils").i("Status bar icons set to ${if (darkIcons) "DARK" else "LIGHT"}")
+        Timber.tag("ComposeUtils")
+                .i("Status bar icons set to ${if (darkIcons) "DARK" else "LIGHT"}")
     }
 }
 
+@Composable
+fun IntSize.toDpSize(): DpSize {
+    return DpSize(
+            width = with(LocalDensity.current) { width.toDp() },
+            height = with(LocalDensity.current) { height.toDp() }
+    )
+}
+
+@Composable
+fun InvisibleSpacer(
+    componentSize: IntSize,
+    modifier: Modifier = Modifier
+) {
+    Spacer(
+            modifier = modifier
+                    .size(with(LocalDensity.current) {
+                        componentSize.toDpSize()
+                    })
+    )
+}
