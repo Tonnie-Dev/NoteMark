c4b530d9ed4fcf52d5cf7d6699adb15b2ed4d166
Tonnie <vontonnie@gmail.com>
2025-08-11 20:41:48 +0300

    Update AppHeader.kt

--- app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppHeader.kt
+++ app/src/main/java/com/tonyxlab/notemark/presentation/core/components/AppHeader.kt
@@ -9,22 +9,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.tonyxlab.notemark.presentation.core.utils.spacing
-
 
 @Composable
 fun Header(
     @StringRes title: Int,
     @StringRes subTitle: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    textAlignment: TextAlign = TextAlign.Start
 ) {
     Column(modifier = modifier) {
         AppTitleText(
                 modifier = Modifier.fillMaxWidth(),
-                text = stringResource(id = title)
+                text = stringResource(id = title),
+                textAlign = textAlignment
         )
         Spacer(modifier = Modifier.height(MaterialTheme.spacing.spaceSingleDp * 6))
-        AppCaptionText(text = stringResource(id = subTitle))
+        AppCaptionText(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = subTitle),
+                textAlign = textAlignment
+        )
     }
 }
 
